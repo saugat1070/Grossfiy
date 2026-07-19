5cd41467c4d5ff477cbc035617ade194ead25341
saugat1070 <saugatgiri1070@gmail.com>
2026-07-19 19:44:29 +0545

    tabs page

---
+++ src/app/(tabs)/insights.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function InsightScreen() {
+  return (
+    <SafeAreaView>
+      <Text className="text-3xl text-foreground">InsightScreen</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
